506e1c54b3a129f465ab7ae31d20b93e52166713
Ivgenium <genrymorgon@mail.ru>
2025-08-10 10:54:59 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
@@ -15,7 +15,7 @@ public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PREFIX = "/vacancies?page=";
     private static final String SUFFIX = "&q=Java%20developer&type=all";
-    private static final int COUNT = 1;
+    private static final int COUNT = 5;
     private final DateTimeParser dateTimeParser;
 
     public HabrCareerParse(DateTimeParser dateTimeParser) {
@@ -44,6 +44,7 @@ public class HabrCareerParse implements Parse {
                     var post = new Post();
                     post.setTitle(vacancyName);
                     post.setLink(link);
+                    post.setDescription(description);
                     post.setTime(dateTimeParser.parse(dateTime));
                     result.add(post);
                 });
